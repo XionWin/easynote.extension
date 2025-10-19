4d13de648245698e6338ecf83c903f0107dfd02b
XionWin <xionwin@hotmail.com>
2025-10-19 15:31:25 -0400

    Update

--- index-chunk.js
+++ index-chunk.js
@@ -1,1 +1,1 @@
-import{E as a}from"./features-animation-chunk.js";import"./settings-chunk.js";var i=a;export{i as default};
+import{E as a}from"./features-animation-chunk.js";import"./translation-setting-utility-chunk.js";var i=a;export{i as default};
